f4151514e56644d00bb8471324cbec77abfb1304
aSipz <115708388+aSipz@users.noreply.github.com>
2022-10-17 20:46:22 +0300

    Finished Dungeones

--- 02_Fundamentals/03_Arrays/29.DungeonestDark.js
+++ 02_Fundamentals/03_Arrays/29.DungeonestDark.js
@@ -3,27 +3,42 @@ function dungeons(inputString) {
     let coins = 0;
     let healingNum = 0;
     let inputArray = inputString[0].split('|');
-    console.log(inputArray);
+    let roomNum = 0;
+    let isSuccessful = true;
     for (let i = 0; i < inputArray.length; i++) {
         let roomArray = inputArray[i].split(' ');
-        if (roomArray[0] == 'portion') {
-            if (health += Number(roomArray[1]) <= 100) {
+        if (roomArray[0] == 'potion') {
+            if ((health + Number(roomArray[1])) <= 100) {
                 health += Number(roomArray[1]);
                 healingNum = Number(roomArray[1]);
             } else {
+                healingNum = 100 - health;
                 health = 100;
-                healingNum = 100 - health - Number(roomArray[1]);
             }
+            roomNum++;
             console.log(`You healed for ${healingNum} hp.`);
             console.log(`Current health: ${health} hp.`);
         } else if (roomArray[0] == 'chest') {
             coins += Number(roomArray[1]);
+            roomNum++;
             console.log(`You found ${Number(roomArray[1])} coins.`);
         } else {
+            roomNum++;
             if (health > Number(roomArray[1])) {
-                log
+                console.log(`You slayed ${roomArray[0]}.`);
+                health -= Number(roomArray[1]);
+            } else {
+                console.log(`You died! Killed by ${roomArray[0]}.`);
+                console.log(`Best room: ${roomNum}`);
+                isSuccessful = false;
+                break;
             }
         }
     }
+    if (isSuccessful) {
+        console.log(`You've made it!`);
+        console.log(`Coins: ${coins}`);
+        console.log(`Health: ${health}`);
+    }
 }
-dungeons(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"]);
+dungeons(["cat 10|potion 30|orc 10|chest 10|snake 25|chest 110"]);
