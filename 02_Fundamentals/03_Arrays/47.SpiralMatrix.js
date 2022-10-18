68a81e54d50c334a086aba884b3b7be7c233784e
aSipz <115708388+aSipz@users.noreply.github.com>
2022-10-18 17:14:36 +0300

    Updated Spiral

--- 02_Fundamentals/03_Arrays/47.SpiralMatrix.js
+++ 02_Fundamentals/03_Arrays/47.SpiralMatrix.js
@@ -1,16 +1,38 @@
 function spiralMatrix(dim1, dim2) {
     let numToPrint = 1;
-    let rowArray = []
-    let firstRowArr = [];
-    // first row
+    let spiralArray = []
+    //create matrix
     for (let i = 0; i < dim1; i++) {
-        rowArray.push(numToPrint + i);
+        let row = [];
+        for (let j = 0; j < dim2; j++) {
+            row.push([]);
+        }
+        spiralArray.push(row);
+    }
+    for (let i = 0; i <= 0; i++) {
+        for (let j = 0; j <= dim1; j++) {
+            spiralArray[i][j] = numToPrint;
+            numToPrint++;
+        }
+    }
+    for (let i = 1; i < dim2; i++) {
+        let j = dim1;
+        spiralArray[i][j] = numToPrint;
         numToPrint++;
     }
-    // last row
-    for (i = 0; i < dim1; i++) {
-        numToPrint -=
-        rowArray.push(numToPrint + i)
-    }
+    for ()
+    console.log(spiralArray.join('\n'));
+    // let rowArray = []
+    // let firstRowArr = [];
+    // // first row
+    // for (let i = 0; i < dim1; i++) {
+    //     rowArray.push(numToPrint + i);
+    //     numToPrint++;
+    // }
+    // // last row
+    // for (i = 0; i < dim1; i++) {
+    //     numToPrint -=
+    //     rowArray.push(numToPrint + i)
+    // }
 }
-spiralMatrix();
+spiralMatrix(5,5);
