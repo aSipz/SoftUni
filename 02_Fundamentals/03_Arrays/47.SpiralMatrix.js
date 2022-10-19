238c5be7816a81ef1989386f058931222239abaa
aSipz <115708388+aSipz@users.noreply.github.com>
2022-10-19 11:00:15 +0300

    Resolved Spiral

--- 02_Fundamentals/03_Arrays/47.SpiralMatrix.js
+++ 02_Fundamentals/03_Arrays/47.SpiralMatrix.js
@@ -1,38 +1,64 @@
 function spiralMatrix(dim1, dim2) {
     let numToPrint = 1;
     let spiralArray = []
+    let rotations = 0;
     //create matrix
-    for (let i = 0; i < dim1; i++) {
+    for (let i = 0; i < dim2; i++) {
         let row = [];
-        for (let j = 0; j < dim2; j++) {
+        for (let j = 0; j < dim1; j++) {
             row.push([]);
         }
         spiralArray.push(row);
     }
-    for (let i = 0; i <= 0; i++) {
-        for (let j = 0; j <= dim1; j++) {
-            spiralArray[i][j] = numToPrint;
-            numToPrint++;
+    //fill matrix
+    while (numToPrint <= dim1 * dim2) {
+        //first row
+        for (let i = 0 + rotations; i <= 0 + rotations; i++) {
+            for (let j = 0 + rotations; j < dim1 - rotations; j++) {
+                if (numToPrint <= dim1 * dim2) {
+                    spiralArray[i][j] = numToPrint;
+                    numToPrint++;
+                } else {
+                    break;
+                }
+            }
         }
+        //last column
+        for (let i = 1 + rotations; i < dim2 - rotations; i++) {
+            let j = dim1 - 1 - rotations;
+            if (numToPrint <= dim1 * dim2) {
+                spiralArray[i][j] = numToPrint;
+                numToPrint++;
+            } else {
+                break;
+            }
+        }
+        //last row
+        for (let i = dim2 - 1 - rotations; i < dim2 - rotations; i++) {
+            for (let j = dim1 - 2 - rotations; j >= 0 + rotations; j--) {
+                if (numToPrint <= dim1 * dim2) {
+                    spiralArray[i][j] = numToPrint;
+                    numToPrint++;
+                } else {
+                    break;
+                }
+            }
+        }
+        //first column
+        for (let i = dim2 - 2 - rotations; i > 0 + rotations; i--) {
+            let j = 0 + rotations;
+            if (numToPrint <= dim1 * dim2) {
+                spiralArray[i][j] = numToPrint;
+                numToPrint++;
+            } else {
+                break;
+            }
+        }
+        rotations++;
     }
-    for (let i = 1; i < dim2; i++) {
-        let j = dim1;
-        spiralArray[i][j] = numToPrint;
-        numToPrint++;
+    for (let i = 0; i < dim2; i++) {
+        let rowArr = spiralArray[i]
+        console.log(rowArr.join(' '));
     }
-    for ()
-    console.log(spiralArray.join('\n'));
-    // let rowArray = []
-    // let firstRowArr = [];
-    // // first row
-    // for (let i = 0; i < dim1; i++) {
-    //     rowArray.push(numToPrint + i);
-    //     numToPrint++;
-    // }
-    // // last row
-    // for (i = 0; i < dim1; i++) {
-    //     numToPrint -=
-    //     rowArray.push(numToPrint + i)
-    // }
 }
-spiralMatrix(5,5);
+spiralMatrix(4, 10);
