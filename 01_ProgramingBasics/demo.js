b600517c8bdfe378d1806d71d3f66b79e3a5e9f2
aSipz <115708388+aSipz@users.noreply.github.com>
2022-10-24 21:00:11 +0300

    Exam

--- 01_ProgramingBasics/demo.js
+++ 01_ProgramingBasics/demo.js
@@ -1,14 +1,4 @@
-function demo(input) {
-    let x1 = parseFloat(input[0]);
-    let y1 = parseFloat(input[1]);
-    let x2 = parseFloat(input[2]);
-    let y2 = parseFloat(input[3]);
-    let x = parseFloat(input[4]);
-    let y = parseFloat(input[5]);
-    if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
-        console.log("Inside");
-    } else {
-        console.log("Outside");
-    }
-}   
-demo([]);
+function demo() {
+console.log(5 / 0);  
+} 
+demo();
