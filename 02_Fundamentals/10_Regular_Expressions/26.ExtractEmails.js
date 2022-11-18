50fb28e79daedc19e1d65213054f4a41d59500e7
aSipz <115708388+aSipz@users.noreply.github.com>
2022-11-18 23:02:31 +0200

    solved email extract

--- 02_Fundamentals/10_Regular_Expressions/26.ExtractEmails.js
+++ 02_Fundamentals/10_Regular_Expressions/26.ExtractEmails.js
@@ -1,6 +1,6 @@
 function extract(text) {
-    let pattern = /\b[A-Za-z0-9]+[A-Za-z0-9.\-_]*(?<![.\-_])@[a-z]+\-?[a-z]+\.[a-z]+\.?[a-z]*\b/g;
+    let pattern = /(?<=\s|^)([A-Za-z0-9]+[A-Za-z0-9.\-_]*(?<![.\-_])@[a-z]+\-?[a-z]+(\.[a-z]+)+)(?=,|\s|\.$|\.\s\w+|$)/g;
     let emails = text.match(pattern);
     console.log(emails.join('\n'));
 }
-extract('Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.');
+extract('Many users @ SoftUni confuse email addresses. We @ Softuni.BG provide high-quality training @ home or @ class. –- steve.parker@softuni.de.');
