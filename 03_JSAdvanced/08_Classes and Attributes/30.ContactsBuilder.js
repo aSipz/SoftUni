356427c33da9480a0f240ad9578fb9fcd4515b70
aSipz <115708388+aSipz@users.noreply.github.com>
2022-12-23 23:04:45 +0200

    1

--- 03_JSAdvanced/08_Classes and Attributes/30.ContactsBuilder.js
+++ 03_JSAdvanced/08_Classes and Attributes/30.ContactsBuilder.js
@@ -4,36 +4,48 @@ class Contact {
         this.lastName = lastName;
         this.phone = phone;
         this.email = email;
-        this.online = false;
+        this._online = false;
+        this.article = document.createElement('article');
+    }
+    get online() { return this._online }
+    set online(value) {
+        this._online = value;
+        if (!this.article.children[0]) {
+            return;
+        }
+        if (this._online) {
+            this.article.children[0].classList.add('online');
+        } else {
+                this.article.children[0].classList.remove('online');
+        }
     }
     render(id) {
         let outerDiv = document.getElementById(id);
-        let article = document.createElement('article');
-        let button = createBtn();
-        console.log(button);
         let innerHtml = `<div class="title">` +
             `${this.firstName} ${this.lastName}` +
-            button +
+            '<button>&#8505;</button>' +
             `</div>` +
             `<div class="info" style="display: none;">` +
             `<span>&phone; ${this.phone}</span>` +
             `<span>&#9993; ${this.email}</span>` +
             ` </div>`;
-        article.innerHTML = innerHtml;
-        outerDiv.appendChild(article);
+        this.article.innerHTML = innerHtml;
+        this.article.addEventListener('click', reveal);
+        if (this.online) {
+            this.article.children[0].classList.add('online');
+        }
+        outerDiv.appendChild(this.article);
 
-        function createBtn() {
-            let btn = document.createElement('button');
-            btn.textContent = '&#8505;';
-            btn.addEventListener('click', reveal)
-            function reveal(e) {
-                if (e.target.parentElement.style.display == 'none') {
-                    e.target.parentElement.style.display = 'block';
-                } else {
-                    e.target.parentElement.style.display = 'none'
-                }
+        function reveal(e) {
+            if (e.target.nodeName != 'BUTTON') {
+                return;
             }
-            return btn;
+            let hiddenDiv = e.target.parentElement.parentElement.children[1];
+            if (hiddenDiv.style.display == 'none') {
+                hiddenDiv.style.display = 'block';
+            } else {
+                hiddenDiv.style.display = 'none'
+            }
         }
     }
 }
@@ -43,8 +55,8 @@ let contacts = [
     new Contact("Maria", "Petrova", "0899 987 654", "mar4eto@abv.bg"),
     new Contact("Jordan", "Kirov", "0988 456 789", "jordk@gmail.com")
   ];
+  contacts[0].online = true
   contacts.forEach(c => c.render('main'));
   
   // After 1 second, change the online status to true
   setTimeout(() => contacts[1].online = true, 2000);
-  
