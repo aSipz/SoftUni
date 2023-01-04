52ea7fbb41e15aec7bd8c505bf3487de7d719f8b
aSipz <115708388+aSipz@users.noreply.github.com>
2023-01-04 17:19:34 +0200

    Updated game

--- 03_JSAdvanced/11_SideScrollingGame/main.js
+++ 03_JSAdvanced/11_SideScrollingGame/main.js
@@ -3,12 +3,15 @@ const gameScore = document.querySelector('.game-score');
 const gameArea = document.querySelector('.game-area');
 const gameOver = document.querySelector('.game-over');
 const gamePoints = gameScore.querySelector('.points');
-
-gameStart.addEventListener('click', onGameStart);
-document.addEventListener('keydown', onKeyDown);
-document.addEventListener('keyup', onKeyUp);
+const startBtn = gameStart.querySelector('.start');
+const difficultySelector = Array.from(gameStart.querySelectorAll('.difficulty'));
 
 let keys = {};
+let difficulty = {
+    'Easy': 1,
+    'Medium': 4,
+    'Hard': 8
+}
 let player = {
     x: 150,
     y: 100,
@@ -20,11 +23,12 @@ let game = {
     speed: 2,
     movingMultiplier: 4,
     fireBallMultiplier: 5,
-    fireInterval: 1000,
+    fireInterval: 500,
     cloudSpawnInterval: 3000,
     bugSpawnInterval: 1000,
     bugSpeedMultiplier: 3,
-    bugKillBonus: 2000
+    bugKillBonus: 2000,
+    difficultyMultiplier: 0
 };
 let scene = {
     score: 0,
@@ -33,8 +37,34 @@ let scene = {
     isActiveGame: true
 };
 
-function onGameStart() {
+gameStart.addEventListener('click', onGameStart);
+gameStart.addEventListener('click', chooseDifficulty);
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+function onGameStart(e) {
+    if (e.target.className != 'start') {
+        return;
+    }
+
+    // check for difficulty
+    let isSelected = false;
+    for (const selector of difficultySelector) {
+        if (selector.className.includes('chosen')) {
+            isSelected = true;
+            break;
+        }
+    }
+    if (!isSelected) {
+        let messageDiv = document.createElement('div');
+        messageDiv.textContent = 'Please select difficulty';
+        messageDiv.classList.add('error');
+        gameStart.appendChild(messageDiv);
+        return;
+    }
+
     gameStart.classList.add('hide');
+    gameScore.classList.remove('hide');
     gameArea.style.cursor = 'none';
     const wizard = document.createElement('div');
     wizard.classList.add('wizard');
@@ -93,7 +123,7 @@ function gameAction(timestamp) {
     });
 
     // add bugs
-    if (timestamp - scene.lastBugSpawn > game.bugSpawnInterval + 5000 * Math.random()) {
+    if (timestamp - scene.lastBugSpawn > game.bugSpawnInterval + 5000 * Math.random() / game.difficultyMultiplier) {
         let bug = document.createElement('div');
         bug.classList.add('bug');
         bug.x = gameArea.offsetWidth - 60;
@@ -151,15 +181,70 @@ function gameAction(timestamp) {
     scene.score++;
     gamePoints.textContent = scene.score;
 
+    // speed increase
+    if (timestamp % 10000 == 0) {
+        game.speed += 0.1;
+    }
+
     if (scene.isActiveGame) {
         window.requestAnimationFrame(gameAction);
     }
 }
 
+function chooseDifficulty(e) {
+    if (e.target.className != 'difficulty') {
+        return;
+    }
+    difficultySelector.forEach(element => {
+        if (e.target == element) {
+            element.classList.add('chosen');
+            game.difficultyMultiplier = difficulty[element.textContent];
+            let errorDiv = e.target.parentElement.parentElement.querySelector('.error');
+            if (errorDiv) {
+                errorDiv.parentElement.removeChild(errorDiv);
+            }
+        } else {
+            element.classList.remove('chosen');
+        }
+    });
+}
+
 function gameOverAction() {
     scene.isActiveGame = false;
     gameOver.classList.remove('hide');
-    gameArea.style.cursor = '';
+    let result = document.createElement('div');
+    result.classList.add('result');
+    result.innerText = 'Result:\n' + `${scene.score} points`;
+    gameOver.appendChild(result);
+    gameArea.style.cursor = 'default';
+    gameScore.classList.add('hide');
+
+    // best results
+    let resultString = localStorage.getItem('results');
+    if (!resultString) {
+        resultString = '';
+    }
+    resultString += ' ' + scene.score;
+    resultString.trim();
+    localStorage.setItem('results', resultString);
+    let first = resultString
+        .split(' ')
+        .map(Number)
+        .sort((a, b) => b - a)
+        .slice(0, 3);
+
+    let bestResultDiv = document.createElement('div');
+    bestResultDiv.innerText = first.join('\n');
+    gameOver.appendChild(bestResultDiv);
+
+    //start again
+    let startAgain = document.createElement('div');
+    startAgain.classList.add('reload');
+    startAgain.textContent = 'Try again';
+    startAgain.addEventListener('click', () => {
+        location.reload();
+    })
+    gameOver.appendChild(startAgain);
 }
 
 function isCollision(firstElement, secondElement) {
