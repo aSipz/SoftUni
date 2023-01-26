f6573cbbe1de09aada626bb35a21e4be64564e94
aSipz <115708388+aSipz@users.noreply.github.com>
2023-01-26 17:40:51 +0200

    up

---
+++ 04_JSApplications/01. Modules/lib.js
@@ -0,0 +1,3 @@
+export {default as page} from '../node_modules/page/page.mjs';
+export {html , render} from '../node_modules/lit-html/lit-html.js';
+export {repeat} from '../node_modules/lit-html/directives/repeat.js';
