f6f1130803f3882b3b5ef0ad68249b72ec9dd6de
aSipz <115708388+aSipz@users.noreply.github.com>
2023-05-09 22:25:33 +0300

    Update 01.DataDefinitionAndDataTypesExercise.sql

--- 09_MySQL/01.DataDefinitionAndDataTypesExercise.sql
+++ 09_MySQL/01.DataDefinitionAndDataTypesExercise.sql
@@ -18,10 +18,15 @@ RENAME COLUMN town_id TO id;
 
 ALTER TABLE `minions`
 ADD COlUMN town_id INT,
-ADD FOREIGN KEY (`town_id`) REFERENCES `towns`(`id`);
+ADD CONSTRAINT fk_minions_towns
+FOREIGN KEY (`town_id`) REFERENCES `towns`(`id`);
 
-INSERT INTO `towns` (name)
-VALUES ('Sofia'), ('Plovdiv'), ('Varna');
+INSERT INTO `towns` (id, name)
+VALUES (1,'Sofia'), (2,'Plovdiv'), (3,'Varna');
 
-INSERT INTO `minions` (name, age, town_id)
-VALUES ('Kevin', 22, 1), ('Bob', 15, 3), ('Steward', NULL, 2);
+INSERT INTO `minions` (id,name, age, town_id)
+VALUES (1,'Kevin', 22, 1), (2,'Bob', 15, 3), (3,'Steward', NULL, 2);
+
+TRUNCATE TABLE `minions`;
+
+DROP TABLE `minions`, `towns`;
